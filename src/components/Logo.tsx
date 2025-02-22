b7ccab7613d931d375012d2e2ad803e3dff0c117
Marko <marcosvelasquezvela123@gmail.com>
2025-02-22 09:33:13 -0400

    parche bug star

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,14 +5,21 @@ import HeroTitle from "./HeroTitle";
 
 const Logo: React.FC = () => {
   const star = useRef<HTMLImageElement>(null);
+
   useEffect(() => {
-    gsap.to(star.current, {
+    if (!star.current) return;
+
+    const element = star.current;
+
+    // Animaciones iniciales
+    gsap.to(element, {
       duration: 2,
-      filter: "brightness(0) saturate(100%) invert(38%) sepia(67%) saturate(4679%) hue-rotate(243deg) brightness(105%) contrast(103%)",
+      filter:
+        "brightness(0) saturate(100%) invert(38%) sepia(67%) saturate(4679%) hue-rotate(243deg) brightness(105%) contrast(103%)",
       ease: "power3.out",
     });
 
-    gsap.to(star.current, {
+    gsap.to(element, {
       rotate: 360,
       duration: 2,
       ease: "power2.out",
@@ -20,40 +27,39 @@ const Logo: React.FC = () => {
       repeatDelay: 2,
     });
 
-
-    if (star.current) {
-      star.current.addEventListener("mouseenter", () => {
-        gsap.to(star.current, {
-          ease: "power2.out",
-          scale: 1.3,
-          duration: 0.5,
-          opacity: 0, 
-        });
+    // Funciones para los eventos
+    const handleMouseEnter = () => {
+      gsap.timeline().to(element, {
+        rotateY: 180,
+        ease: "power2.out",
+        scale: 1.1,
+        duration: 1,
+  
+        onStart: () => {
+          element.removeEventListener("mouseenter", handleMouseEnter);
+        }
+      }).to(element, {
+        rotateY: -180,
+        scale: 1,
+        delay: 0.5,
+        duration: 1,
+        onComplete: () => {
+          element.addEventListener("mouseenter", handleMouseEnter);
+        }
       });
+    };
+    element.addEventListener("mouseenter", handleMouseEnter);
 
-      star.current.addEventListener("mouseleave", () => {
-        gsap.to(star.current, {
-          scale: 1, // Vuelve al tamaño original
-          duration: 0.3,
-          opacity: 1, // Duración de la animación
-        });
-      });
-    }
   }, []);
 
   return (
     <div className="flex items-center">
-      
-        
-
       <Image
-        id="starImage"
         src="/estrella.svg"
         alt="logo estrella"
         width={275}
         height={100}
         ref={star}
-       
       />
       <HeroTitle title="CUTIE MARK" subtitle="Software Solutions" />
     </div>
