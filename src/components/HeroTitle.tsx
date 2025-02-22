e42abc490e84e05140461ae4a1f02dde135b70bc
Marko <marcosvelasquezvela123@gmail.com>
2025-02-22 07:58:39 -0400

    animacion y correcion

--- src/components/HeroTitle.tsx
+++ src/components/HeroTitle.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import SplitType from "split-type";
 
@@ -9,10 +9,43 @@ interface HeroTitleProps {
 }
 
 const HeroTitle: React.FC<HeroTitleProps> = ({ title, subtitle }) => {
+  const divRef = useRef(null);
   useEffect(() => {
     const text = new SplitType(".hero-title", { types: "words,chars" }) || {
       chars: [],
     };
+    const subtitle = new SplitType(".hero-subtitle", { types: "words,chars" }) || {
+      chars: [],
+    };
+
+    if (subtitle.chars) {
+      subtitle.chars.forEach((char, index) => {
+        const charTl = gsap.timeline();
+        charTl.from(char, {
+          x: 200,
+          duration: 0.75,
+          delay: index * 0.03,
+          opacity: 0,
+          ease: "power2.out",
+        });
+        charTl.to(char, {
+          color: "rgb(209, 179,255)",
+          duration: 0.4,
+          delay: index * 0.1,
+          ease: "linear",
+          repeat: -1,
+          repeatDelay: 4,
+        });
+        charTl.to(char, {
+          color: "rgb(140,83,255)",
+          duration: 0.4,
+          delay: 0.40,
+          ease: "linear",
+          repeat: -1,
+          repeatDelay: 4,
+        });
+      });
+    }
 
     if (text.chars) {
       text.chars.forEach((char, index) => {
@@ -72,7 +105,7 @@ const HeroTitle: React.FC<HeroTitleProps> = ({ title, subtitle }) => {
     <div className="flex flex-col">
       <h1 className="items-center italic text-9xl mt-8 hero-title">{title}</h1>
       {subtitle && (
-        <h2 className="font-inter text-lightViolet text-4xl font-normal text-center mt-2">
+        <h2 className="font-inter text-lightViolet text-4xl font-normal text-center mt-2 hero-subtitle" ref={divRef}>
           {subtitle}
         </h2>
       )}
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import HeroTitle from "./HeroTitle";
 
 const Logo: React.FC = () => {
+  const star = useRef<HTMLImageElement>(null);
   useEffect(() => {
-    gsap.to("#starImage", {
+    gsap.to(star.current, {
       duration: 2,
       filter: "brightness(0) saturate(100%) invert(38%) sepia(67%) saturate(4679%) hue-rotate(243deg) brightness(105%) contrast(103%)",
       ease: "power3.out",
     });
 
-    gsap.to("#starImage", {
+    gsap.to(star.current, {
       rotate: 360,
       duration: 2,
       ease: "power2.out",
@@ -19,10 +20,10 @@ const Logo: React.FC = () => {
       repeatDelay: 2,
     });
 
-    const element = document.querySelector("#starImage");
-    if (element) {
-      element.addEventListener("mouseenter", () => {
-        gsap.to(element, {
+
+    if (star.current) {
+      star.current.addEventListener("mouseenter", () => {
+        gsap.to(star.current, {
           ease: "power2.out",
           scale: 1.3,
           duration: 0.5,
@@ -30,8 +31,8 @@ const Logo: React.FC = () => {
         });
       });
 
-      element.addEventListener("mouseleave", () => {
-        gsap.to(element, {
+      star.current.addEventListener("mouseleave", () => {
+        gsap.to(star.current, {
           scale: 1, // Vuelve al tamaño original
           duration: 0.3,
           opacity: 1, // Duración de la animación
@@ -51,6 +52,7 @@ const Logo: React.FC = () => {
         alt="logo estrella"
         width={275}
         height={100}
+        ref={star}
        
       />
       <HeroTitle title="CUTIE MARK" subtitle="Software Solutions" />
