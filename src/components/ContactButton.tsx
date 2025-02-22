09772a7495c2e455300dd74e4bebc2e6a82d5156
Marko <marcosvelasquezvela123@gmail.com>
2025-02-22 17:16:22 -0400

    mejora de animaciones

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -18,64 +18,49 @@ const ContactButton: React.FC<ContactButtonProps> = ({ handleClick }) => {
     gsap.set(bgEffect, { scaleX: 0, transformOrigin: "left center" });
     gsap.set(text, { color: "#662ECC" });
 
-    // Animación de latido (palpitación)
-    gsap.to(buttonRef.current, {
-      scale: 1.1,
-      duration: 0.7,
-      repeat: -1,
-      ease: "sine.out",
-      repeatDelay: 2,
-    });
-
-    gsap.to(buttonRef.current, {
-      scale: 1,
-      duration: 0.7,
-      delay: 0.7,
-      repeat: -1,
-      ease: "sine.out",
-      repeatDelay: 2,
-    });
-
     const handleHover = () => {
       gsap.to(buttonRef.current, {
         borderColor: "#ffffff",
         ease: "power2.out",
-        duration: 0.4,
+        duration: 0.5,
       });
       gsap.to(bgEffect, {
         scaleX: 1,
         scaleY: 1.1,
-        duration: 0.4,
+        duration: 0.5,
         ease: "power2.out",
-        delay: 0.4,
+        delay: 0.5,
       });
       gsap.to(text, {
         color: "#FFFFFF",
         duration: 0.3,
         ease: "power2.out",
-        delay: 0.4,
+        delay: 0.5,
         onComplete: () => {
-          gsap.to(bgEffect, { scaleX: 0, duration: 0.4, ease: "power2.out", delay: 2.4 });
+          gsap.to(bgEffect, {
+            scaleX: 0,
+            duration: 0.5,
+            ease: "power2.out",
+            delay: 1.1,
+          });
           gsap.to(text, {
-            delay: 2.4,
+            delay: 1.1,
             color: "#662ECC",
-            duration: 0.3,
+            duration: 0.5,
             ease: "power2.out",
           });
           gsap.to(buttonRef.current, {
-            delay: 2.8,
+            delay: 1.5,
             borderColor: "#662ECC",
-            duration: 0.4,
+            duration: 0.5,
           });
-        }
+        },
       });
-    }; 
+    };
 
-    buttonRef.current.addEventListener("mouseenter", handleHover );
+    buttonRef.current.addEventListener("mouseenter", handleHover);
 
-    buttonRef.current.addEventListener("mouseleave", () => {
-      
-    });
+    buttonRef.current.addEventListener("mouseleave", () => {});
   }, []);
 
   return (
