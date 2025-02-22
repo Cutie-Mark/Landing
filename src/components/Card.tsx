970093e19cc2f13cc5009c0fa2af1ec752b58274
Marko <marcosvelasquezvela123@gmail.com>
2025-02-22 15:11:48 -0400

    mejora de colores

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,24 +11,24 @@ const Card = ({
     className?: string;
 }) => {
     return (
-        <div
-            className={` bg-gradient-to-b from-[rgb(28,16,51)] to-[#543199] rounded-xl sm:rounded-3xl p-3 sm:p-4 md:p-6  flex flex-col items-center w-full justify-center ${className}`}
-        >
-            {icon && (
-                <div className="md:p-2 ">
-                    <Image
-                        src={icon}
-                        className="object-fill w-11 sm:w-14"
-                        alt="icon"
-                        width={60}
-                        height={60}
-                    />
-                </div>
-            )}
-            <div className="h-full flex items-center justify-center">
-                <p className="text-white text-center mt-2 md:mt-5">{text}</p>
-            </div>
+      <div
+        className={` bg-gradient-to-b from-[#8C52FF] to-[#662ECC] rounded-xl sm:rounded-3xl p-3 sm:p-4 md:p-6  flex flex-col items-center w-full justify-center ${className}`}
+      >
+        {icon && (
+          <div className="md:p-2 ">
+            <Image
+              src={icon}
+              className="object-fill w-11 sm:w-14"
+              alt="icon"
+              width={60}
+              height={60}
+            />
+          </div>
+        )}
+        <div className="h-full flex items-center justify-center">
+          <p className="text-white text-center mt-2 md:mt-5">{text}</p>
         </div>
+      </div>
     );
 };
 
