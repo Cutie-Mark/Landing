f48e932dcbb1b58aa82a7fd6d1cd85bd4a8d5676
Marko <marcosvelasquezvela123@gmail.com>
2025-02-23 18:21:17 -0400

    mas correcciones

--- src/components/Nosotros.tsx
+++ src/components/Nosotros.tsx
@@ -75,23 +75,38 @@ const Nosotros = () => {
   }, []);
 
   return (
-    <div
-      id="nosotros"
-      className="flex justify-center w-full bg-[url(/bg-image-1.png)] h-screen bg-cover relative z-0" // Añadido relative
-    >
-      <div className="snap-always flex-col md:flex-row flex justify-center items-center gap-20 sm:gap-10 w-4/5">
-        <h2
-          ref={boxRef}
-          className="text-white text-3xl sm:text-5xl font-bold w-full"
-        >
-          ¿Quienes Somos?
-        </h2>
-        <div className="relative w-full">
-          <div
-            ref={divRef}
-            className="bg-white rounded-xl p-5 sm:p-10 w-full origin-left"
+    <div className="bg-[url(/bg-image-1.png)] bg-cover z-0">
+      <div
+        id="nosotros"
+        className="flex justify-center w-full  min-h-screen h-auto bg-cover relative z-0 mt-10 mb-10"
+      >
+        <div className="snap-always flex-col md:flex-row flex justify-center items-center gap-20 sm:gap-10 w-4/5">
+          <h2
+            ref={boxRef}
+            className="text-white text-3xl sm:text-5xl font-bold w-full"
           >
-            <p className=" opacity-0">
+            ¿Quienes Somos?
+          </h2>
+          <div className="relative w-full">
+            <div
+              ref={divRef}
+              className="bg-white rounded-xl p-5 sm:p-10 w-full origin-left"
+            >
+              <p className=" opacity-0">
+                Somos una empresa apasionada por la innovación y la tecnología,
+                dedicada a transformar negocios a través de soluciones digitales
+                que impulsan su crecimiento. Nos especializamos en desarrollar,
+                comercializar e implementar herramientas tecnológicas de
+                vanguardia, diseñadas para optimizar procesos, mejorar la
+                productividad y fortalecer la competitividad de las empresas en
+                un mundo cada vez más digital.
+              </p>
+            </div>
+
+            <p
+              ref={textRef}
+              className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-100 p-5"
+            >
               Somos una empresa apasionada por la innovación y la tecnología,
               dedicada a transformar negocios a través de soluciones digitales
               que impulsan su crecimiento. Nos especializamos en desarrollar,
@@ -101,21 +116,9 @@ const Nosotros = () => {
               mundo cada vez más digital.
             </p>
           </div>
-
-          <p
-            ref={textRef}
-            className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-100 p-5"
-          >
-            Somos una empresa apasionada por la innovación y la tecnología,
-            dedicada a transformar negocios a través de soluciones digitales que
-            impulsan su crecimiento. Nos especializamos en desarrollar,
-            comercializar e implementar herramientas tecnológicas de vanguardia,
-            diseñadas para optimizar procesos, mejorar la productividad y
-            fortalecer la competitividad de las empresas en un mundo cada vez
-            más digital.
-          </p>
         </div>
       </div>
+      <div className="h-5"></div>
     </div>
   );
 };
